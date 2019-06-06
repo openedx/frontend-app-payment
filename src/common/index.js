26e2d7df72f1aa74fc38d054d953e8b5d8820620
Douglas Hall <dhall@edx.org>
2019-06-06 13:30:36 -0400

    Render ErrorPage component via app-level error boundary.

--- src/common/index.js
+++ src/common/index.js
@@ -1,8 +1,10 @@
 import * as utils from './utils';
+import ErrorBoundary from './components/ErrorBoundary';
 import PageLoading from './components/PageLoading';
 import { configureUserAccountApiService, fetchUserAccount } from './actions';
 
 export {
+  ErrorBoundary,
   PageLoading,
   utils,
   configureUserAccountApiService,
