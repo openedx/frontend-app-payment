b0cd37d8babbf0ba48ca65cdff2216eded69170b
julianajlk <julianajlk@gmail.com>
2019-07-24 13:32:01 -0400

    fix: add coupon sendTrackEvent (#116)
    ARCH-941

--- src/payment/coupon/CouponForm.test.jsx
+++ src/payment/coupon/CouponForm.test.jsx
@@ -4,6 +4,7 @@ import renderer from 'react-test-renderer';
 import { mount } from 'enzyme';
 import configureMockStore from 'redux-mock-store';
 import { IntlProvider, injectIntl, configure as configureI18n } from '@edx/frontend-i18n';
+import analytics from '@edx/frontend-analytics';
 
 import { configuration } from '../../environment';
 import messages from '../../i18n';
@@ -12,6 +13,10 @@ import ConnectedCouponForm, { CouponForm } from './CouponForm';
 
 jest.mock('@edx/frontend-logging', () => ({
   logError: jest.fn(),
+}));
+
+jest.mock('@edx/frontend-analytics', () => ({
+  sendTrackEvent: jest.fn(),
 }));
 
 configureI18n(configuration, messages);
@@ -89,6 +94,27 @@ describe('CouponForm', () => {
 
     wrapper.find('form').simulate('submit');
     expect(addCoupon).toHaveBeenCalledWith('DEMO25');
+  });
+
+  it('sends coupon track data on form submit', () => {
+    const IntlCouponForm = injectIntl(CouponForm);
+    const wrapper = mount((
+      <IntlProvider locale="en">
+        <IntlCouponForm
+          onSubmit={() => {}}
+          code="DEMO25"
+        />
+      </IntlProvider>
+    ));
+    const couponFormComponent = wrapper.find(CouponForm).first().instance();
+    analytics.sendTrackEvent = jest.fn();
+    const eventName = 'edx.bi.ecommerce.basket.voucher_applied';
+    const eventProps = {
+      type: 'click',
+      category: 'voucher-application',
+    };
+    couponFormComponent.handleSubmitButtonClick(eventName, eventProps);
+    expect(analytics.sendTrackEvent).toHaveBeenCalledWith(eventName, eventProps);
   });
 
   it('should call removeCoupon on form submit', () => {
