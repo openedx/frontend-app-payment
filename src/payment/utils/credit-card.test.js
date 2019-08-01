239c1258c7963a5d2fcc7ad65461ed0e31b65d45
David Joy <davidjoy@users.noreply.github.com>
2019-08-01 14:22:15 -0400

    Returning null for unexpected card types. (#183)

---
+++ src/payment/utils/credit-card.test.js
@@ -0,0 +1,37 @@
+import { faCcVisa } from '@fortawesome/free-brands-svg-icons';
+
+import { getCardIcon, getCardTypeId } from './credit-card';
+
+describe('credit-card', () => {
+  describe('getCardIcon', () => {
+    it('should return null for unsupported card types', () => {
+      const cardIcon = getCardIcon('36');
+      expect(cardIcon).toBeNull();
+    });
+
+    it('should return null for a nonsense card type', () => {
+      const cardIcon = getCardIcon('foo');
+      expect(cardIcon).toBeNull();
+    });
+
+    it('should return the expected icon for a supported card type', () => {
+      expect(getCardIcon('41')).toEqual(faCcVisa);
+    });
+  });
+
+  describe('getCardTypeId', () => {
+    it('should return null for unsupported card types', () => {
+      const cardIcon = getCardTypeId('36');
+      expect(cardIcon).toBeNull();
+    });
+
+    it('should return null for a nonsense card type', () => {
+      const cardIcon = getCardTypeId('foo');
+      expect(cardIcon).toBeNull();
+    });
+
+    it('should return the expected icon for a supported card type', () => {
+      expect(getCardTypeId('41')).toEqual('001');
+    });
+  });
+});
