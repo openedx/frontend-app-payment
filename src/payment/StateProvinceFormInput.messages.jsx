b97d9a3aa86576866d904e8d93eb209031cb5c1f
David Joy <djoy@edx.org>
2019-08-07 10:22:26 -0400

    Cease to use angle brackets in messages.
    They break transifex.

--- src/payment/StateProvinceFormInput.messages.jsx
+++ src/payment/StateProvinceFormInput.messages.jsx
@@ -3,7 +3,7 @@ import { defineMessages } from '@edx/frontend-i18n';
 const messages = defineMessages({
   'payment.card.holder.information.state.options.empty': {
     id: 'payment.card.holder.information.state.options.empty',
-    defaultMessage: '<Choose state/province>',
+    defaultMessage: 'Choose state/province',
     description: 'Option for empty value on card holder information state/province field.',
   },
 });
