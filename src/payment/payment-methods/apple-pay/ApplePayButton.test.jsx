fdfdca6eaf3b8f875dc3d3fb1b492c3a7aee9823
Adam Butterworth <abutterworth@edx.org>
2019-08-09 14:27:41 -0400

    tests: remove now misplaced tests

--- src/payment/payment-methods/apple-pay/ApplePayButton.test.jsx
+++ src/payment/payment-methods/apple-pay/ApplePayButton.test.jsx
@@ -1,6 +1,5 @@
 /* eslint-disable global-require */
 import React from 'react';
-import { mount } from 'enzyme';
 import renderer from 'react-test-renderer';
 import { IntlProvider, configure as configureI18n } from '@edx/frontend-i18n';
 import ApplePayButton from './ApplePayButton';
@@ -24,93 +23,16 @@ jest.mock('@edx/frontend-analytics', () => ({
   sendTrackEvent: jest.fn(),
 }));
 
-jest.mock('@edx/frontend-logging', () => ({
-  logError: jest.fn(),
-}));
-
-// import { logError } from '@edx/frontend-logging'; // eslint-disable-line import/first
-
-
-// describe('<ApplePayButton />', () => {
-//   it('should render properly', () => {
-//     const tree = renderer
-//       .create((
-//         <IntlProvider locale="en">
-//           <ApplePayButton
-//             lang="en"
-//             title="Pay with Apple Pay"
-//           />
-//         </IntlProvider>
-//       ))
-//       .toJSON();
-
-//     expect(tree).toMatchSnapshot();
-//   });
-// });
 describe('<ApplePayButton />', () => {
   it('should render properly', () => {
     const tree = renderer
-      .create(<ApplePayButton totalAmount={10} lang="en" title="Pay with Apple Pay" />)
+      .create((
+        <IntlProvider locale="en">
+          <ApplePayButton />
+        </IntlProvider>
+      ))
       .toJSON();
 
     expect(tree).toMatchSnapshot();
-  });
-
-  it('should perform an apple pay transaction on click', () => {
-    const component = <ApplePayButton totalAmount={10} lang="en" title="Pay with Apple Pay" />;
-    const wrapper = mount(component);
-    analytics.sendTrackEvent = jest.fn();
-    const eventName = 'edx.bi.ecommerce.basket.payment_selected';
-    const eventProps = {
-      type: 'click',
-      category: 'checkout',
-      paymentMethod: 'Apple Pay',
-    };
-    wrapper.find('button').simulate('click');
-    expect(analytics.sendTrackEvent).toHaveBeenCalledWith(eventName, eventProps);
-    expect(performApplePayPayment).toHaveBeenCalledWith({
-      totalAmount: 10,
-      onPaymentBegin: wrapper.prop('onPaymentBegin'),
-      onPaymentComplete: wrapper.prop('onPaymentComplete'),
-      onMerchantValidationFailure: wrapper.prop('onMerchantValidationFailure'),
-      onPaymentAuthorizationFailure: wrapper.prop('onPaymentAuthorizationFailure'),
-      onPaymentCancel: wrapper.prop('onPaymentCancel'),
-    });
-  });
-
-  it('should bail if it cannot submit for some reason', () => {
-    const component = <ApplePayButton lang="en" title="Pay with Apple Pay" />;
-    const wrapper = mount(component);
-    analytics.sendTrackEvent = jest.fn();
-
-    performApplePayPayment.mockClear();
-
-    wrapper.find('button').simulate('click');
-
-    expect(analytics.sendTrackEvent).not.toHaveBeenCalled();
-    expect(performApplePayPayment).not.toHaveBeenCalled();
-  });
-
-  describe('with exploding ApplePaySession canMakePayments', () => {
-    let error;
-
-    beforeEach(() => {
-      error = new Error();
-      global.ApplePaySession.canMakePayments = () => {
-        throw error;
-      };
-    });
-
-    afterEach(() => {
-      global.ApplePaySession.canMakePayments = () => true;
-    });
-
-    it('should log an error if it catches one in constructor', () => {
-      const component = <ApplePayButton lang="en" title="Pay with Apple Pay" />;
-
-      mount(component);
-
-      expect(logError).toHaveBeenCalledWith(error);
-    });
   });
 });
