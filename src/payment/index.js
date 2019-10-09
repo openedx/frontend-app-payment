391b85e1067749e40e969ca41f344caaee8d8f6b
David Joy <djoy@edx.org>
2019-10-09 13:46:00 -0400

    Creating a component called EcommerceRedirect
    It’s used to catch 404s (in the MFE) and forward them on to the ecommerce service.  This will be used in our route configuration.

--- src/payment/index.js
+++ src/payment/index.js
@@ -2,4 +2,5 @@ export { default as PaymentPage } from './PaymentPage';
 export { default as reducer } from './data/reducers';
 export { default as saga } from './data/sagas';
 export { storeName } from './data/selectors';
+export { default as EcommerceRedirect } from './EcommerceRedirect';
 export { default as responseInterceptor } from './responseInterceptor';
