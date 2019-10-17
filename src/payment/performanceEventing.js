bad281bd5ec4d6858c52108fe0c1769740f75b7c
Brandon Baker <bbaker@edx.org>
2019-10-17 15:22:58 -0400

    Change Cart loading default to fix event
    Make loading default to true so that OrderSummary doesn't render until
    it actually is loaded.
    Additionally has some eslint fixes that were not caught in travis

--- src/payment/performanceEventing.js
+++ src/payment/performanceEventing.js
@@ -8,7 +8,11 @@ export function markPerformanceIfAble(markName) {
 // Returns performance timing properties to send in events
 export function getPerformanceProperties() {
   return {
-    emitted_at: Date.now() / 1000, // Seconds since epoch, for comparing timing on backend vs. frontend
-    performance_timestamp: performance.now(), // Milliseconds since time origin. See https://developer.mozilla.org/en-US/docs/Web/API/Performance/now
+    // Seconds since epoch, for comparing timing on backend vs. frontend
+    emitted_at: Date.now() / 1000,
+
+    // Milliseconds since time origin.
+    // See https://developer.mozilla.org/en-US/docs/Web/API/Performance/now
+    performance_timestamp: performance.now(),
   };
 }
