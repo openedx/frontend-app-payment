223dc4ed3da0e2c0a7a66f4e1eb9c91e07659b8b
Ben Holt <bholt+github@edx.org>
2020-04-01 14:50:04 -0400

    Add proof-of-concept tracking event; also make it easier to generate_static_pages that are not minified for debugging

--- public/static-checkout/generate_static_pages.py
+++ public/static-checkout/generate_static_pages.py
@@ -1,17 +1,17 @@
 import csv
 # Combine minified html/css/js into one string
 with open('dist/minified.html', 'r') as file:
-    html = file.read().replace('\n', '')
+    html = file.read()
 with open('dist/minified.css', 'r') as file:
-    css = file.read().replace('\n', '')
+    css = file.read()
 with open('dist/minified.js', 'r') as file:
-    js = file.read().replace('\n', '')
+    js = file.read()
 # Insert css into the html
 style_index = html.find('<style>') + 7
 html = html[:style_index] + css + html[style_index:]
 # Insert javascript into the html
 script_index = html.find('<script>') + 8
-html = html[:script_index] + css + html[script_index:]
+html = html[:script_index] + js + html[script_index:]
 
 # Read in course data and generate a page for each course
 with open('courselist.csv', newline='') as csvfile:
