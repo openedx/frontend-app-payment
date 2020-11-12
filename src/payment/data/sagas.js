5585c0b14220a949cc113cc0988c201de55f2739
Ben Holt <bholt+github@edx.org>
2020-11-12 08:54:00 -0500

    PCI-119: disable old payment form (#433)
    * Disable old form without deleting code
    
    * Delete microform waffle checks
    
    * Comment-out microformEnabled props up and down the tree
    
    * Continue sending microformEnabled on telemetry
    
    * Remove old tests that relied on overriding to the old form
    
    * Comment-out tests that require faking the microform

--- src/payment/data/sagas.js
+++ src/payment/data/sagas.js
@@ -2,7 +2,7 @@ import {
   call, put, takeEvery, select, delay,
 } from 'redux-saga/effects';
 import { stopSubmit } from 'redux-form';
-import { camelCaseObject, convertKeyNames, isWaffleFlagEnabled } from './utils';
+import { camelCaseObject, convertKeyNames } from './utils';
 import { MESSAGE_TYPES } from '../../feedback/data/constants';
 
 // Actions
@@ -154,9 +154,7 @@ export function* handleFetchCaptureKey() {
     yield put(microformStatus(STATUS_LOADING)); // we are refreshing the capture key
     const result = yield call(PaymentApiService.getCaptureKey);
     yield put(captureKeyDataReceived(result)); // update redux store with capture key data
-    if (isWaffleFlagEnabled('payment.cybersource.flex_microform_enabled', false)) {
-      yield put(captureKeyStartTimeout()); // only start the timer if we're using the capture key
-    }
+    yield put(captureKeyStartTimeout()); // only start the timer if we're using the capture key
   } catch (error) {
     yield call(handleErrors, error, true);
   } finally {
@@ -213,7 +211,7 @@ export function* handleSubmitPayment({ payload }) {
     yield put(clearMessages()); // Don't leave messages floating on the page after clicking submit
     yield put(submitPayment.request());
     let paymentMethodCheckout = paymentMethods[method];
-    if (method === 'cybersource' && isWaffleFlagEnabled('payment.cybersource.flex_microform_enabled', false)) {
+    if (method === 'cybersource') {
       paymentMethodCheckout = checkoutWithToken;
     }
     const basket = yield select(state => ({ ...state.payment.basket }));
