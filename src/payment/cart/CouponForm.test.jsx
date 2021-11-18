dbe08559f884cfc0324b72a3e89b4ae887d87f7f
Phillip Shiu <pshiu@users.noreply.github.com>
2021-11-18 09:55:32 -0500

    chore: [REV-1551] clean up dependencies for Renovate (#510)
    * chore: run npm audit fix
    
    * chore: upgrade to @edx/frontend-build@8.1.3 for npm audit
    
    * chore: remove purgecss-webpack-plugin
    
    Required to upgrade @edx/frontend-build.
    
    See https://github.com/edx/frontend-build/releases/tag/v7.0.0
    
    * chore: upgrade to immer@9.0.6 for npm audit
    
    * chore: upgrade to @edx/paragon@16.16.0 for npm audit
    
    * chore: upgrade to mozjpeg@7.1.1 for npm audit
    
    Note: There is a known issue in mozjpeg@7.1.1:
    
    On GitHub, see: imagemin/mozjpeg-bin!67
    
    This may throw errors in the dev build. However, since it is a
    devDependency, it should not affect the prod build.
    
    Workarounds:
    * Pin mozjpeg@7.1.0 locally.
    * `apt-get install nasm` in your frontend-app-payment devstack shell.
    
    Note mozjpeg@7.1.0 contains 1 high vulnerability.
    
    * chore: upgrade to html-webpack-plugin@5.3.2 to fix htmlWebpackPluginAlterChunks error
    
    * chore: upgrade react (&c) for npm audit
    
    * fix: unpin all repos and npm update for renovate prep
    
    * chore: upgrade to audit-ci@5 & husky@7
    
    * chore: upgrade to paragon@16.17
    
    * fix: refresh snapshots after dependency upgrades
    
    * chore: upgrade to form-urlencoded@6.0.5
    
    * chore: upgrade to es-check@6.1.0 for npm audit
    
    * chore: whitelist audit findings after manual review
    
    * fix: sync Makefile precommit with package.json
    
    * fix: add PCI compliance reminders
    
    * chore: update snapshots
    
    * fix: convert react-test-renderer tests to enzyme
    
    Paragon <Input> components employ an accessibility test called
    checkHasLabel() that uses forwardRefs.
    
    checkHasLabel() is set to run only in devstack.
    
    react-test-renderer is natively incompatible with forwardRefs:
    
        https://reactjs.org/docs/test-renderer.html#ideas
    
    Convert react-test-renderer tests to enzyme tests so a jsdom makes
    forwardRefs available for the devstack accessibility tests.
    
    * fix: have CI do npm audit last
    
    Audits usually are addressed after all tests have passed.
    
    If audit precede test checks, information about test failure is obscured
    by incoming audit findings.
    
    * fix: remove depcheck-id'ed unused dependencies
    
    * fix: upgrade husky
    
    * fix: run npm upgrade & npm dedup
    
    * fix: update owner & reflect removal of Purgecss in README.md
    
    * fix: reword PCI compliance impact check
    
    * chore: npm-force-resolutions to upgrade deps in lockfile

--- src/payment/cart/CouponForm.test.jsx
+++ src/payment/cart/CouponForm.test.jsx
@@ -4,11 +4,15 @@ import renderer from 'react-test-renderer';
 import { mount } from 'enzyme';
 import defaultsDeep from 'lodash.defaultsdeep';
 import configureMockStore from 'redux-mock-store';
+import { createSerializer } from 'enzyme-to-json';
 import { IntlProvider } from '@edx/frontend-platform/i18n';
 import { sendTrackEvent } from '@edx/frontend-platform/analytics';
 
 import CouponForm from './CouponForm';
 import { addCoupon, removeCoupon } from '../data/actions';
+
+// run enzyme JSON serializer using options compatible with prior snapshots
+expect.addSnapshotSerializer(createSerializer({ mode: 'deep', noKey: true }));
 
 jest.mock('@edx/frontend-platform/analytics', () => ({
   sendTrackEvent: jest.fn(),
@@ -79,7 +83,7 @@ describe('CouponForm', () => {
 
   it('should render a form when there is no coupon', () => {
     const component = renderCouponFormWithState(mockStore(baseState));
-    const tree = renderer.create(component).toJSON();
+    const tree = mount(component);
     expect(tree).toMatchSnapshot();
   });
 
