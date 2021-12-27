7e54935f072d6795c09eac7ec03a64b7e382381c
Ben Holt <bholt+github@edx.org>
2021-12-27 10:57:15 -0500

    temp!: temporarily required ascii characters in name fields

--- src/payment/checkout/payment-form/PaymentForm.jsx
+++ src/payment/checkout/payment-form/PaymentForm.jsx
@@ -57,6 +57,10 @@ export class PaymentFormComponent extends React.Component {
 
     const errors = {
       ...this.validateRequiredFields(requiredFields),
+      ...this.validateAsciiNames(
+        firstName,
+        lastName,
+      ),
       ...this.validateCardDetails(
         cardExpirationMonth,
         cardExpirationYear,
@@ -132,6 +136,20 @@ export class PaymentFormComponent extends React.Component {
       && parseInt(cardExpirationYear, 10) === currentYear
     ) {
       errors.cardExpirationMonth = 'payment.form.errors.card.expired';
+    }
+
+    return errors;
+  }
+
+  validateAsciiNames(firstName, lastName) {
+    const errors = {};
+
+    if (
+      firstName
+      && lastName
+      && !/[A-Za-z]/.test(firstName + lastName)
+    ) {
+      errors.firstName = 'payment.form.errors.ascii.name';
     }
 
     return errors;
