372f88a1a8726b76f070b9a4102482d5c848cb78
Raza Dar <5585262+mrazadar@users.noreply.github.com>
2023-07-18 03:24:30 +0500

    feat: handles the happy flow

--- src/subscription/secure-3d/secure-3d-modal/Secure3dModal.jsx
+++ src/subscription/secure-3d/secure-3d-modal/Secure3dModal.jsx
@@ -1,29 +1,40 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import PropTypes from 'prop-types';
+import { useDispatch, useSelector } from 'react-redux';
 
 import {
   ModalDialog,
 } from '@edx/paragon';
-import { useSelector } from 'react-redux';
+import {
+  PaymentElement,
+} from '@stripe/react-stripe-js';
 
 import { subscriptionStatusSelector } from '../../data/status/selectors';
 import { detailsSelector } from '../../data/details/selectors';
+
+import {
+  handleSuccessful3DS,
+  handleUnSuccessful3DS,
+} from '../../data/status/actions';
 
 /**
  * Secure3DModal
  * This modal implements the 3DS functionality for
  * both trial and non-trial purchases. It uses the setupIntent
- * for trial and paymentIntent function for non-trial
+ * for trial and paymentIntent for non-trial
  * purchases in order to load 3DS details inside an iFrame modal
  */
 export const Secure3DModal = ({ stripe, elements }) => {
-  const { status, confirmationClientSecret } = useSelector(subscriptionStatusSelector);
-  const { isTrialEligible } = useSelector(detailsSelector);
+  const dispatch = useDispatch();
+  const modalRef = useRef(false);
   const [isOpen, setOpen] = useState(false);
+
+  const { isTrialEligible } = useSelector(detailsSelector);
+  const { status, confirmationClientSecret } = useSelector(subscriptionStatusSelector);
 
   /**
    * loadSecureDetails
-   */
+  */
   const loadSecureDetails = (url) => {
     setTimeout(() => {
       const container = document.getElementById('secure-3ds-wrapper');
@@ -35,24 +46,82 @@ export const Secure3DModal = ({ stripe, elements }) => {
     });
   };
 
+  /**
+   * retrieveSetupIntent
+   * call this method with subscription in trial mode
+   */
   const retrieveSetupIntent = async () => {
     const response = await stripe.retrieveSetupIntent(confirmationClientSecret);
     if (response.error) { throw response.error; }
     return response.setupIntent;
   };
 
+  /**
+   * retrievePaymentIntent
+   * call this method with subscription in non-trial mode
+   */
   const retrievePaymentIntent = async () => {
     const response = await stripe.retrievePaymentIntent(confirmationClientSecret);
     if (response.error) { throw response.error; }
     return response.paymentIntent;
   };
 
+  /**
+   * on3DSComplete
+   * callback to handle 3DS completion state
+   */
+  const on3DSComplete = async () => {
+    // Hide the 3DS UI
+    if (modalRef.current === 'active') {
+      // completed 3DS, set to inActive so multiple state updates doesn't trigger the 3DS update
+      modalRef.current = 'inActive';
+      const error = new Error('Could not complete the payment', { cause: 'requires_payment_method' });
+      try {
+        const fetchPaymentDetails = isTrialEligible ? retrieveSetupIntent : retrievePaymentIntent;
+        const paymentDetails = await fetchPaymentDetails();
+        if (paymentDetails.status === 'succeeded') {
+          // Show your customer that the payment has succeeded
+          dispatch(handleSuccessful3DS({ status: paymentDetails.status }));
+        } else if (paymentDetails.status === 'requires_payment_method') {
+          // clear the stripe elements and ask user to submit new details
+          // Authentication failed, prompt the customer to enter another payment method
+          const paymentElement = elements.getElement(PaymentElement);
+          paymentElement.clear();
+          paymentElement.focus();
+          dispatch(handleUnSuccessful3DS({ error }));
+        } else {
+          // Status unknown
+          dispatch(handleUnSuccessful3DS({ error }));
+        }
+        window.removeEventListener('message', null);
+        setOpen(false);
+      } catch (e) {
+        setOpen(false);
+        dispatch(handleUnSuccessful3DS({ error }));
+        window.removeEventListener('message', null);
+      }
+    }
+  };
+
+  /**
+   * fetchSecureDetails
+   * fetches the 3D secure details
+   */
   const fetchSecureDetails = async () => {
     try {
       const fetchPaymentDetails = isTrialEligible ? retrieveSetupIntent : retrievePaymentIntent;
       const paymentDetails = await fetchPaymentDetails();
       loadSecureDetails(paymentDetails.next_action.redirect_to_url.url);
-      setOpen(true);
+      setOpen(() => {
+        // attempting 3ds, because state changes doesn't work with window listeners
+        modalRef.current = 'active';
+        return true;
+      });
+      window.addEventListener('message', (ev) => {
+        if (ev.data === '3DS-authentication-complete') {
+          on3DSComplete();
+        }
+      }, false);
     } catch (e) {
       // TODO: log the error to segment
       throw new Error(`Error loading 3D secure details): ${e.message}`);
@@ -63,38 +132,10 @@ export const Secure3DModal = ({ stripe, elements }) => {
     if (status === 'requires_action') {
       fetchSecureDetails();
     }
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [status]);
 
-  const on3DSComplete = async () => {
-    // Hide the 3DS UI
-    setOpen(false);
-    try {
-      const fetchPaymentDetails = isTrialEligible ? retrieveSetupIntent : retrievePaymentIntent;
-      const paymentDetails = await fetchPaymentDetails();
-      if (paymentDetails.status === 'succeeded') {
-        // Show your customer that the payment has succeeded
-        console.log('make stripe-fulfill api call');
-      } else if (paymentDetails.status === 'requires_payment_method') {
-        elements.clear();
-        elements.focus();
-
-        // Authentication failed, prompt the customer to enter another payment method
-        console.log('clear the stripe elements and ask user to submit new details');
-      }
-    } catch (e) {
-      // TODO: log the error to segment
-      throw new Error(`Error loading 3D secure details): ${e.message}`);
-    }
-  };
-
-  window.addEventListener('message', (ev) => {
-    if (ev.data === '3DS-authentication-complete') {
-      on3DSComplete();
-    }
-  }, false);
-
-  if (!isOpen) { return null; }
+  if (!isOpen || !stripe || !elements) { return null; }
 
   return (
     <ModalDialog
@@ -114,11 +155,17 @@ Secure3DModal.propTypes = {
   stripe: PropTypes.shape({
     retrievePaymentIntent: () => {},
     retrieveSetupIntent: () => {},
-  }).isRequired,
+  }),
   elements: PropTypes.shape({
     clear: () => {},
     focus: () => {},
-  }).isRequired,
+    getElement: () => {},
+  }),
+};
+
+Secure3DModal.defaultProps = {
+  stripe: null,
+  elements: null,
 };
 
 export default Secure3DModal;
